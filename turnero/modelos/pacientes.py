ae948fa99badf1ba6cc07db57c8d04d3e5cc2126
not-tomi <peruilhtomas22@gmail.com>
2023-12-14 22:48:28 +0100

    ahora si funka

--- turnero/modelos/pacientes.py
+++ turnero/modelos/pacientes.py
@@ -1,12 +1,11 @@
 import requests
+import csv
 
 # Lista de pacientes en memoria (diccionarios)
 pacientes = []
 
-
-# Función para cargar pacientes desde la API randomuser
-def cargar_pacientes_desde_api(cantidad=10):
-
+# Función para cargar pacientes desde la API randomuser y escribir en CSV
+def cargar_pacientes_desde_api_y_guardar_en_csv(cantidad=10):
     url = f'https://randomuser.me/api/?results={cantidad}&inc=id,dni,name,phone,email,location'
     response = requests.get(url)
     data = response.json()
@@ -24,5 +23,18 @@ def cargar_pacientes_desde_api(cantidad=10):
         }
         pacientes.append(paciente)
 
+    # Escribir los datos en pacientes.csv
+    with open('pacientes.csv', mode='w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ["id", "dni", "nombre", "apellido", "telefono", "email", "direccion_calle", "direccion_numero"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        # Escribir el encabezado solo si el archivo está vacío
+        if csv_file.tell() == 0:
+            writer.writeheader()
+
+        # Escribir cada paciente en una nueva fila
+        for paciente in pacientes:
+            writer.writerow(paciente)
+
 # Cargar algunos pacientes iniciales desde la API al inicio de la aplicación
-cargar_pacientes_desde_api(5)
+cargar_pacientes_desde_api_y_guardar_en_csv(5)
